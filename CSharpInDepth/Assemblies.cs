04727724e14619cd93032a6af1ad998d84ff6c92
Praveen Kumar Sinha <38722697+SinhaPraveen97@users.noreply.github.com>
2026-03-14 09:08:43 +0530

    Add files via upload

---
+++ CSharpInDepth/Assemblies.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CSharpInDepth
+{
+   
+    class Assemblies
+    {
+        [CompilationRelaxations(89)]
+        //C:\D Drive\B2\Agility\MVC\OOPSConcept\CSharpInDepth>csc /target:library /out:Assemblies.dll Assemblies.cs
+        static void Main()
+        {
+
+        }
+    }
+}
