db8f5373878397a2bdbb78c65fcc54648f1a4c40
Praveen Kumar Sinha <38722697+SinhaPraveen97@users.noreply.github.com>
2026-03-14 09:12:05 +0530

    Add files via upload

---
+++ OOPsConceptPrep2026/MutableVsImmutable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPsConceptPrep2026
+{
+    public class MutableVsImmutable
+    {
+        public static void Main()
+        {
+            string a = "Hello";
+            string b = "Hello";
+            string c = b;
+           StringBuilder sb = new StringBuilder("Hello");
+            sb.Append(" World");
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+            Console.WriteLine(c.GetHashCode());
+            c += " World"; // Creates a new string object
+            string d = "Hello World";
+            Console.WriteLine(c.GetHashCode());
+            Console.WriteLine(d.GetHashCode());
+            Console.WriteLine(sb.ToString()); // Hello World
+            Console.WriteLine(Object.ReferenceEquals(a, b)); // True
+           
+        }
+    }
+}
